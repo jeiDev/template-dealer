d28b70b23f459ab6fd9c5245748027a04bace17b
JeiDevp <joseanyeloserrano@gmail.com>
2019-03-18 00:21:39 -0400

    Design home

--- assets/js/app.js
+++ assets/js/app.js
@@ -67,4 +67,66 @@ class createDOM {
         header.appendChild(center)
     }
 
+    animationBanner(){
+        var time = null
+        let parent = document.getElementById("bannerAnimation");
+        let items = document.querySelectorAll(".item-banner");
+        let num = 0;
+        let active = true;
+        let pag = document.querySelectorAll("#paginationBanner>.pag")
+
+        interval()
+        function interval(){
+            time = setInterval(()=>{
+                if(num < (items.length-1) && active){
+                    num += 1
+                    console.log(num)
+                    document.querySelector(".page-active").classList.remove("page-active")
+                    document.getElementById(`pag${num}`).classList.add("page-active")
+                    parent.style.left = `-${num * 100}vw`
+                }else if(active){
+                    num = 0
+                    document.querySelector(".page-active").classList.remove("page-active")
+                    document.getElementById(`pag${num}`).classList.add("page-active")
+                    parent.style.left = `-${0}vw`
+                }
+            },5000)
+        }
+
+        for (let i = 0; i < pag.length; i++) {
+            pag[i].addEventListener("click", () => {
+                clearInterval(time)
+                num = +pag[i].getAttribute("pag")
+                document.querySelector(".page-active").classList.remove("page-active")
+                document.getElementById(`pag${num}`).classList.add("page-active")
+                parent.style.left = `-${num * 100}vw`
+                interval()
+            })
+        }
+
+    }
+
+    footer(){
+        let footer = document.querySelector("footer")
+        let max_width = document.createElement("div")
+        let column1 = document.createElement("div")
+        let column2 = document.createElement("div")
+
+        max_width.setAttribute("class", "max-width")
+        column1.setAttribute("class", "column1")
+        column2.setAttribute("class", "column2")
+
+
+        column1.innerHTML = ` <div class="left"><h2>SIGN UP FOR AMAZING OFFERS</h2><p>EXCLUSIVE ACCESS FOR OFFERS AND PROMOTIONS</p></div><div class="right"><div class="box-input"><input type="text" placeholder="Start typing your e-mail address..."><div>SEND</div></div></div>`
+        column2.innerHTML = ` <div class="left"><p>Copyright © 2019</p><p>All Rights Reserved</p></div><div class="right"><p>FOLLOW US:</p><div><i class="fab fa-facebook-f"></i><i class="fab fa-linkedin-in"></i><i class="fab fa-twitter"></i></div></div>`
+       
+        max_width.appendChild(column1)
+        max_width.appendChild(column2)
+        footer.appendChild(max_width)
+    }
+
+    showCar(id){
+
+    }
+
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,15 @@ document.body.onload = init
 
 
 function init(){
+    /*DECLARED CLASS CREATE*/
     let create = new createDOM()
+
+    //Create header
     create.header()
+
+    //Animation banner
+    create.animationBanner()
+
+    //Create footer
+    create.footer()
 }
