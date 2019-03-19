1db9a669fae115e3a722256b237d56a44133d454
JeiDevp <joseanyeloserrano@gmail.com>
2019-03-19 14:53:21 -0400

    Design and animation view card

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,7 +29,7 @@ class createDOM {
                     section.appendChild(a)
                 })
 
-               
+
             } else {
                 let iconPhone = document.createElement("div")
                 let phone = document.createElement("div")
@@ -37,7 +37,7 @@ class createDOM {
                 let lenguage = document.createElement("div")
                 let bar2 = document.createElement("div")
                 let login = document.createElement("i")
-                
+
                 iconPhone.style.marginRight = "5px"
                 phone.style.marginRight = "10px"
                 bar1.style.marginRight = "10px"
@@ -67,7 +67,7 @@ class createDOM {
         header.appendChild(center)
     }
 
-    animationBanner(){
+    animationBanner() {
         var time = null
         let parent = document.getElementById("bannerAnimation");
         let items = document.querySelectorAll(".item-banner");
@@ -76,21 +76,20 @@ class createDOM {
         let pag = document.querySelectorAll("#paginationBanner>.pag")
 
         interval()
-        function interval(){
-            time = setInterval(()=>{
-                if(num < (items.length-1) && active){
+        function interval() {
+            time = setInterval(() => {
+                if (num < (items.length - 1) && active) {
                     num += 1
-                    console.log(num)
                     document.querySelector(".page-active").classList.remove("page-active")
                     document.getElementById(`pag${num}`).classList.add("page-active")
                     parent.style.left = `-${num * 100}vw`
-                }else if(active){
+                } else if (active) {
                     num = 0
                     document.querySelector(".page-active").classList.remove("page-active")
                     document.getElementById(`pag${num}`).classList.add("page-active")
                     parent.style.left = `-${0}vw`
                 }
-            },5000)
+            }, 5000)
         }
 
         for (let i = 0; i < pag.length; i++) {
@@ -106,7 +105,7 @@ class createDOM {
 
     }
 
-    footer(){
+    footer() {
         let footer = document.querySelector("footer")
         let max_width = document.createElement("div")
         let column1 = document.createElement("div")
@@ -119,14 +118,262 @@ class createDOM {
 
         column1.innerHTML = ` <div class="left"><h2>SIGN UP FOR AMAZING OFFERS</h2><p>EXCLUSIVE ACCESS FOR OFFERS AND PROMOTIONS</p></div><div class="right"><div class="box-input"><input type="text" placeholder="Start typing your e-mail address..."><div>SEND</div></div></div>`
         column2.innerHTML = ` <div class="left"><p>Copyright © 2019</p><p>All Rights Reserved</p></div><div class="right"><p>FOLLOW US:</p><div><i class="fab fa-facebook-f"></i><i class="fab fa-linkedin-in"></i><i class="fab fa-twitter"></i></div></div>`
-       
+
         max_width.appendChild(column1)
         max_width.appendChild(column2)
         footer.appendChild(max_width)
     }
 
-    showCar(id){
+    showCar(id, animation = false) {
+        let car = this.getCars()[id]
+        let parent = document.querySelector("main")
+        let section = document.createElement("section")
+        let maxWidth = document.createElement("div")
+        let title = document.createElement("div")
+        let contentCard = document.createElement("div")
+        let left = document.createElement("div")
+        let right = document.createElement("div")
+        let leftColumn1 = document.createElement("div")
+        let leftColumn2 = document.createElement("div")
+        let contentText = document.createElement("div")
+        let readMore = document.createElement("div")
+        let bar = document.createElement("div")
+        let circleMore = document.createElement("div")
+        let spanMode = document.createElement("span")
+        let class2 = ["title", "price", "color", "call-to-action", "info"]
 
+
+        title.innerText = "INVENTORY"
+        contentText.innerHTML = `<p>${car.description}</p>`
+        circleMore.innerHTML = '<i class="fas fa-angle-down"></i>'
+        spanMode.innerText = "READ MORE"
+
+        /****************************************
+         *** COLUMN 1
+         ****************************************/
+        for (let i = 0; i < 3; i++) {
+            let dom = document.createElement("div")
+
+            if (i == 0) {
+                let minus = document.createElement("i")
+                let plus = document.createElement("i")
+
+                dom.setAttribute("class", "zoon")
+                minus.setAttribute("class", "fas fa-search-minus")
+                plus.setAttribute("class", "fas fa-search-plus")
+
+                dom.appendChild(minus)
+                dom.appendChild(plus)
+            } else if (i == 1) {
+                let animation = document.createElement("div")
+
+                dom.setAttribute("class", "content-img")
+                animation.setAttribute("class", "animation")
+                animation.setAttribute("id", "animation")
+
+                car.imgs.forEach((e, z) => {
+                    let boxImg = document.createElement("div")
+                    let img = document.createElement("img")
+
+                    img.src = `../assets/img/cars/${car.brand.toLowerCase()}-${car.general.model.toLowerCase()}-${car.features.year.toLowerCase()}/${e}-${car.selectColor}.${car.typeImg}`
+
+                    boxImg.setAttribute("class", "box-img")
+
+                    animation.appendChild(boxImg)
+                    boxImg.appendChild(img)
+                })
+
+                dom.appendChild(animation)
+            } else if (i == 2) {
+                setTimeout(() => {
+                    let animation = document.getElementById("animation")
+                    let active = 0
+
+                    dom.setAttribute("class", "pagination-img")
+
+                    car.imgs.forEach((e, z) => {
+                        let pag = document.createElement("div")
+                        let circle = document.createElement("div")
+
+                        pag.setAttribute("class", `pag${z == 0 ? " pag-active-show" : ""}`)
+
+                        if (z == 0) circle.setAttribute("class", `pag-active-show`)
+
+                        dom.appendChild(pag)
+                        pag.appendChild(circle)
+
+                        pag.addEventListener("click", () => {
+                            if(z == active) return
+                            let pagActiveShow = document.querySelectorAll(".pag-active-show")
+                            let parentActive = document.querySelector(".pag.pag-active-show")
+                            let childActive = document.querySelector(".pag.pag-active-show>.pag-active-show")
+                            let newCircle = pag.querySelector("div")
+                            animation.style.top = `-${z * 300}px`
+                            
+                            if (active < z) {
+                                active = z
+                                newCircle.classList.remove("top0")
+                                newCircle.classList.remove("hheight0")
+                                parentActive.style.background = "transparent"
+                                childActive.style.height = "0"
+                                newCircle.classList.add("top0")
+                                setTimeout(() => {
+                                    newCircle.classList.add("pag-active-show")
+                                    pag.classList.add("pag-active-show")
+                                }, 10)
+
+                                setTimeout(()=>{
+                                    newCircle.classList.remove("top0")
+                                },500)
+
+                                for (let i = 0; i < pagActiveShow.length; i++)  pagActiveShow[i].classList.remove("pag-active-show")
+                                return
+                            }
+    
+                            active = z
+                            parentActive.style.background = "transparent"
+                            childActive.classList.add("top0")
+                            childActive.classList.add("hheight0")
+
+                            setTimeout(() => {
+                                newCircle.classList.add("pag-active-show")
+                                pag.classList.add("pag-active-show")
+                            }, 10)
+
+
+                        })
+                    })
+                })
+
+            }
+            leftColumn1.appendChild(dom)
+
+        }
+
+        /****************************************
+         *** COLUMN 2
+         ****************************************/
+        for (let i = 0; i < class2.length; i++) {
+            let dom = document.createElement("div")
+            dom.setAttribute("class", class2[i])
+
+            if(i==0) dom.innerHTML = `<h2>${car.brand}</h2><span>${car.class}-CLASS</span>`
+            else if(i==1) dom.innerHTML = `<span>${car.price}</span>`
+            else if(i==2){
+                let text = document.createElement("div")
+                let circleColor = document.createElement("div")
+                
+                text.setAttribute("class", "text")
+                circleColor.setAttribute("class", "circle-color")
+                text.innerHTML = ` <span>Selected color: </span><b>${car.selectColor}</b>`
+                
+                car.color.forEach(e => {
+                    let color = document.createElement("div")
+
+                    color.setAttribute("class", `c-circle c-circle-${e}${e==car.selectColor ? " c-circle-active" : ""}`)
+                    circleColor.appendChild(color)
+                })
+
+                dom.appendChild(text)
+                dom.appendChild(circleColor)
+            }else if(i==3){
+                let btn = document.createElement("div")
+
+                btn.innerText = "CALL NOW FOR DISCOUNT"
+                btn.setAttribute("class", "btn")
+                
+                dom.appendChild(btn)
+            }else if(i==4){
+                let titles = ["general", "features", "tearms"]
+
+                titles.forEach(e => {
+                    let select = car[e]
+                    let menu = document.createElement("div")
+                    let titleInfo = document.createElement("div")
+                    let contentInfo  = document.createElement("div")
+
+                    titleInfo.innerHTML = `<div>${e.toUpperCase()}</div><i class="fas fa-caret-down"></i>`
+
+                    menu.setAttribute("class", "menus")
+                    titleInfo.setAttribute("class", "title-info")
+                    contentInfo.setAttribute("class", "content-info")
+
+                    Object.keys(select).forEach(key => {
+                        let div = document.createElement("div")
+
+                        div.innerHTML = `<div>${key.toUpperCase()}</div><span>${select[key]}</span>`
+                        
+                        contentInfo.appendChild(div)
+                    })
+
+                    dom.appendChild(menu)
+                    menu.appendChild(titleInfo)
+                    menu.appendChild(contentInfo)
+            
+                })
+            }
+
+            right.appendChild(dom)
+            
+        }
+
+        section.setAttribute("class", "view-car")
+        maxWidth.setAttribute("class", "max-width")
+        title.setAttribute("class", "title")
+        contentCard.setAttribute("class", "content-car")
+        left.setAttribute("class", "left")
+        right.setAttribute("class", "right")
+        leftColumn1.setAttribute("class", "column1")
+        leftColumn2.setAttribute("class", "column2")
+        contentText.setAttribute("class", "content-text")
+        readMore.setAttribute("class", "read-more")
+        bar.setAttribute("class", "bar")
+        circleMore.setAttribute("class", "circle")
+
+        parent.appendChild(section)
+        section.appendChild(maxWidth)
+        maxWidth.appendChild(title)
+        maxWidth.appendChild(contentCard)
+        contentCard.appendChild(left)
+        contentCard.appendChild(right)
+        left.appendChild(leftColumn1)
+        left.appendChild(leftColumn2)
+        leftColumn2.appendChild(contentText)
+        leftColumn2.appendChild(readMore)
+        readMore.appendChild(bar)
+        readMore.appendChild(spanMode)
+        bar.appendChild(circleMore)
     }
 
+    getCars() {
+        return {
+            1: {
+                brand: "Ford",
+                imgs: ["ford-1", "ford-2", "ford-3", "ford-4"],
+                typeImg: "png",
+                color: ["red", "black", "white", "green"],
+                class: "S",
+                selectColor: "black",
+                price: "$ 150 000",
+                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis totam distinctio dolorum natus quidem iste, tenetur quibusdam temporibus consectetur ex facere eos quaerat nobis exercitationem hic quas. Eum, nulla nisi!Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis totam distinctio dolorum natus quidem iste, tenetur quibusdam temporibus consectetur ex facere eos quaerat nobis exercitationem hic quas. Eum, nulla nisi!Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis totam distinctio dolorum natus quidem iste, tenetur quibusdam temporibus consectetur ex facere eos quaerat nobis exercitationem hic quas. Eum, nulla nisi!Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis totam distinctio dolorum natus quidem iste, tenetur quibusdam temporibus consectetur ex facere eos quaerat nobis exercitationem hic quas. Eum, nulla nisi!Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis totam distinctio dolorums",
+                general: {
+                    model: "Explorer",
+                    doors: "5",
+                    seats: "5",
+                    luggage: "2",
+                    transmission: "Auto",
+                    "air aconditioning": "Yes"
+                },
+                features:{
+                    status: "New",
+                    year: "2019",
+                    rubber: "16"
+                },tearms:{
+                    "years of use": "0",
+                    km: "0"
+                }
+
+            }
+        }
+    }
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,41 @@ document.body.onload = init
 
 
 function init(){
-    /*DECLARED CLASS CREATE*/
-    let create = new createDOM()
+    // DECLARE VARIABLES
+    let create = new createDOM() // DECLARED CLASS CREATE
+    let itemsClick =  document.querySelectorAll("#recentCardClick>.car") // SELECT ITEM RECENT CARS
+    let selectPageContent = document.querySelectorAll(".content-page") // SELECT ALL PAGE CONTENT
 
-    //Create header
+    // Create header
     create.header()
 
-    //Animation banner
+    // Animation banner
     create.animationBanner()
 
-    //Create footer
+    // Create footer
     create.footer()
+
+
+    // RECENT CARD CLICK
+    itemsClick.forEach(item => {
+        item.addEventListener("click", () => {
+
+            //SCROLL TOP
+            window.scrollTo({ top: 0, behavior: 'smooth'});
+
+            selectPageContent.forEach((page, i) => {
+                page.classList.add("fade-out")
+                setTimeout(()=>{
+                    page.style.display = "none"
+                    page.classList.remove("fade-out")
+                    if( (i+1) == selectPageContent.length){
+                        
+                        create.showCar(+item.getAttribute("typeId"), true)
+                    } 
+                },500)
+            })
+
+        })
+    })
+  
 }
